78c737618f832c0a407c6d7a30d4d5de737909ad
sevenyoungairye <849033901@qq.com>
2021-02-15 13:24:10 +0800

    使用多线程重入锁的情况下卖票。lambda demo, 函数式接口。

---
+++ Juc/src/main/java/top/bitqian/demo1/Demo01.java
@@ -0,0 +1,92 @@
+package top.bitqian.demo1;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 企业级的多线程代码
+ * 在高内聚低耦合情况下 多线程 操作 资源类
+ * @author echo lovely
+ * @date 2021/2/15 13:08
+ */
+
+class Ticket {
+
+    private int num = 30;
+
+    Lock lock = new ReentrantLock();
+
+    void sale() {
+
+        lock.lock();
+
+        try {
+            if (this.num > 0)
+                System.out.println(Thread.currentThread().getName() + " 当前票数: " + this.num -- + "\t还剩下票数: " + num);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+}
+
+public class Demo01 {
+
+    public static void main(String[] args) {
+
+        Ticket ticket = new Ticket();
+
+        new Thread(() -> {
+
+            for (int i = 0; i < 30; i++) {
+                ticket.sale();
+            }
+
+        } , "A").start();
+
+        new Thread(() -> {
+
+            for (int i = 0; i < 30; i++) {
+                ticket.sale();
+            }
+
+        } , "B").start();
+
+        new Thread(() -> {
+
+            for (int i = 0; i < 30; i++) {
+                ticket.sale();
+            }
+
+        } , "C").start();
+
+    }
+
+    void do1() {
+        Ticket ticket = new Ticket();
+        new MyThread(ticket, "a").start();
+        new MyThread(ticket, "b").start();
+        new MyThread(ticket, "c").start();
+    }
+
+}
+
+class MyThread extends Thread {
+
+    private final Ticket ticket;
+
+    public MyThread(Ticket ticket, String name) {
+        super(name);
+        this.ticket = ticket;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 40; i++) {
+            ticket.sale();
+        }
+    }
+}
---
+++ Juc/src/main/java/top/bitqian/lambda/Demo01.java
@@ -0,0 +1,44 @@
+package top.bitqian.lambda;
+
+/**
+ * @author echo lovely
+ * @date 2021/2/15 11:08
+ */
+
+@FunctionalInterface
+interface Foo {
+
+    // 函数式接口只能有一个未实现方法
+    int add(int x, int y);
+
+    // 可多个静态
+    static int div() {
+
+        return 0;
+    }
+
+    // 可多个default
+    default int mul(int x, int y) {
+        System.out.println("multiply...");
+        return x * y;
+    }
+
+}
+
+public class Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println(Foo.div());
+
+        Foo foo = (int x, int y) -> {
+
+            System.out.println("come in add...");
+            return x + y;
+        };
+
+        System.out.println(foo.add(1, 4));
+
+        System.out.println(foo.mul(9, 9));
+    }
+
+}
