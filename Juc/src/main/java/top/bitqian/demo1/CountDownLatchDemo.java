32e82e4d85cb7cad550a6db6478ef7612454dc76
sevenyoungairye <849033901@qq.com>
2021-02-18 12:27:58 +0800

    使用CountDownLatch指定线程运行的优先级。

---
+++ Juc/src/main/java/top/bitqian/demo1/CountDownLatchDemo.java
@@ -0,0 +1,48 @@
+package top.bitqian.demo1;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author echo lovely
+ * @date 2021/2/18 12:00
+ */
+
+public class CountDownLatchDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        solution();
+
+        // closeDoor();
+
+    }
+
+    private static void solution() throws InterruptedException {
+        // 当指定的线程运行完 主线程开始跑
+        CountDownLatch cdl = new CountDownLatch(6);
+
+        for (int i = 0; i < 6; i++) {
+            new Thread(
+                    () -> {
+                        System.out.println(Thread.currentThread().getName() + "\t" + "left the classroom..");
+                        cdl.countDown();
+                    },
+                    String.valueOf(i)).start();
+        }
+
+        cdl.await();
+
+        System.out.println(Thread.currentThread().getName() + "\t close the door...");
+    }
+
+    private static void closeDoor() {
+        for (int i = 0; i < 6; i++) {
+            new Thread(
+                    () -> System.out.println(Thread.currentThread().getName() + "\t" + "left the classroom.."),
+                    String.valueOf(i)).start();
+        }
+
+        System.out.println(Thread.currentThread().getName() + "\t close the door...");
+    }
+
+}
