c2d52e164b10dc46da05957a3922df18c5cf3a75
sevenyoungairye <849033901@qq.com>
2021-04-11 11:59:56 +0800

    依赖倒转原则。

---
+++ design_pattern/src/main/java/top/bitqian/principle/dependency_inversion/improve/DemoInversion2.java
@@ -0,0 +1,56 @@
+package top.bitqian.principle.dependency_inversion.improve;
+
+/**
+ * @author echo lovely
+ * @date 2021/4/11 10:41
+ * @description <p>
+ *     模仿person接收消息
+ * </p>
+ */
+
+public class DemoInversion2 {
+
+    public static void main(String[] args) {
+
+        /*
+         * 依赖倒转 like 多态
+         */
+        Person p = new Person();
+        p.receive(new Email());
+        p.receive(new YouTelegram());
+
+    }
+
+
+    /**
+     * 获取消息接口
+     */
+    interface IReceiver {
+        String getInfo();
+    }
+
+    static class Email implements IReceiver {
+        @Override
+        public String getInfo() {
+            return "from email: hello, adorable";
+        }
+    }
+
+    static class YouTelegram implements IReceiver {
+        @Override
+        public String getInfo() {
+            return "from t.me: hello, bitQian";
+        }
+    }
+
+    /**
+     * person 接收到消息
+     */
+    static class Person {
+        void receive(IReceiver receiver) {
+            String info = receiver.getInfo();
+            System.out.println(info);
+        }
+    }
+
+}
