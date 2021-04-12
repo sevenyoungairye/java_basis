2f1f4bc66f2bdcaf5637c09e1f87a4adcb170297
sevenyoungairye <849033901@qq.com>
2021-04-12 21:37:10 +0800

    里氏替换原则。

---
+++ design_pattern/src/main/java/top/bitqian/principle/liskov/improve/Liskov2.java
@@ -0,0 +1,58 @@
+package top.bitqian.principle.liskov.improve;
+
+/**
+ * @author echo lovely
+ * @date 2021/4/12 21:17
+ * @description <p>
+ *     更好的解决方案
+ * </p>
+ */
+
+public class Liskov2 {
+
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println("3-2=" + a.fun1(3, 2));
+
+        System.out.println("----------");
+
+        B b = new B();
+        System.out.println("3+2=" + b.fun1(3, 2));
+
+        System.out.println("3-2=" + b.fun2(3, 2));
+    }
+
+
+    static abstract class Base {
+
+    }
+
+    static class A extends Base {
+
+        public int fun1(int a, int b) {
+            return a - b;
+        }
+
+    }
+
+    /**
+     * 通过继承基类 解耦...
+     */
+    static class B extends Base {
+
+        // 依赖 A
+        private final A myA = new A();
+
+        // B与A 无关 不可能认为是重写 add..
+        int fun1(int a, int b) {
+            return a + b;
+        }
+
+        int fun2(int a, int b) {
+            return this.myA.fun1(a, b);
+        }
+
+    }
+
+}
+
