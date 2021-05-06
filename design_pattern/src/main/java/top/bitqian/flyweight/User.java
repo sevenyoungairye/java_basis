513d85de11e11e417d268676d2508d0ae5b426ba
sevenyoungairye <849033901@qq.com>
2021-05-06 22:32:48 +0800

    享元模式。

---
+++ design_pattern/src/main/java/top/bitqian/flyweight/User.java
@@ -0,0 +1,24 @@
+package top.bitqian.flyweight;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author echo lovely
+ * @date 2021/5/6 22:18
+ * @description <p>
+ *   外部状态
+ * </p>
+ */
+
+@Setter
+@Getter
+public class User {
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
---
+++ design_pattern/src/main/java/top/bitqian/flyweight/Website.java
@@ -0,0 +1,15 @@
+package top.bitqian.flyweight;
+
+/**
+ * @author echo lovely
+ * @date 2021/5/6 22:09
+ * @description <p>
+ *
+ * </p>
+ */
+
+public abstract class Website {
+
+    protected abstract void use(User user);
+
+}
