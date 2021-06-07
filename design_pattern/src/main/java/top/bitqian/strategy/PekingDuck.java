fdac68a085ffcd180f645ec9a040edc4f50f98c9
sevenyoungairye <849033901@qq.com>
2021-06-07 18:40:33 +0800

    策略模式。

---
+++ design_pattern/src/main/java/top/bitqian/strategy/PekingDuck.java
@@ -0,0 +1,19 @@
+package top.bitqian.strategy;
+
+/**
+ * @author echo lovely
+ * @date 2021/6/7 16:57
+ * @description peking
+ */
+
+public class PekingDuck extends Duck {
+    @Override
+    public void fly() {
+        System.out.println("peckingDuck could'nt fly..");
+    }
+
+    @Override
+    protected void display() {
+        System.out.println("I'm peking duck...");
+    }
+}
---
+++ design_pattern/src/main/java/top/bitqian/strategy/WildDuck.java
@@ -0,0 +1,15 @@
+package top.bitqian.strategy;
+
+/**
+ * @author echo lovely
+ * @date 2021/6/7 16:56
+ * @description your desc
+ */
+
+public class WildDuck extends Duck {
+
+    @Override
+    protected void display() {
+        System.out.println("I'm wild duck, I can do any..");
+    }
+}
