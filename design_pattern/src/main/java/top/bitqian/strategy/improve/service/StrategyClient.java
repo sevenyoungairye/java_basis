7a108a0659b71c1b2a2f4fef2b70cd9a276930a0
sevenyoungairye <849033901@qq.com>
2021-06-08 09:48:48 +0800

    策略模式。

--- design_pattern/src/main/java/top/bitqian/strategy/improve/service/StrategyClient.java
+++ design_pattern/src/main/java/top/bitqian/strategy/improve/service/StrategyClient.java
@@ -4,6 +4,9 @@ import top.bitqian.strategy.improve.Duck;
 import top.bitqian.strategy.improve.ToyDuck;
 import top.bitqian.strategy.improve.WildDuck;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author echo lovely
  * @date 2021/6/7 19:38
@@ -21,6 +24,18 @@ public class StrategyClient {
         duck = new WildDuck();
         duck.setName("鸭子二号");
         duck.display();
+
+    }
+
+    static void compareDemo() {
+        Integer[] arr = new Integer[] {1, 9, 6, 5};
+
+        // 策略类
+        Comparator<Integer> comparator= Integer::compareTo;
+
+        Arrays.sort(arr, comparator);
+
+        System.out.println(Arrays.toString(arr));
 
     }
 
